c6c38c399490ea4d30761166ce33a6fe8cb72271
[Sona] <[sona16061999@gmail.com]>
2021-08-09 17:28:20 +0530

    Initialised with welcome message

---
+++ PayRollRestAPI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PayRollRestAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee PayRoll REST API!");
+        }
+    }
+}
